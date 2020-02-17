37274be85b5d45472767b231af235cc4c8a02caa
Palash <palash.tripathi92@gmail.com>
2020-02-17 18:35:03 +0530

    Added Authetication for login - JWT token, Authentication Endpoint

--- proman-service/src/main/java/com/upgrad/proman/service/business/AuthenticationService.java
+++ proman-service/src/main/java/com/upgrad/proman/service/business/AuthenticationService.java
@@ -1,10 +1,16 @@
 package com.upgrad.proman.service.business;
 
+import com.upgrad.proman.service.business.JwtTokenProvider;
 import com.upgrad.proman.service.dao.UserDao;
+import com.upgrad.proman.service.entity.UserAuthTokenEntity;
 import com.upgrad.proman.service.entity.UserEntity;
 import com.upgrad.proman.service.exception.AuthenticationFailedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.ZonedDateTime;
 
 @Service
 public class AuthenticationService {
@@ -15,7 +21,9 @@ public class AuthenticationService {
     @Autowired
     private PasswordCryptographyProvider passwordCryptographyProvider;
 
-    public UserEntity authenticate(final String username, final String password) throws AuthenticationFailedException {
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public UserAuthTokenEntity authenticate(final String username, final String password) throws AuthenticationFailedException {
 
         UserEntity userEntity = userDao.getUserByEmail(username);
 
@@ -29,13 +37,26 @@ public class AuthenticationService {
 
 
         if(encryptedPassword.equals(userEntity.getPassword())){
-            // generate JWT send this as a response
-            // Header - alg: "HS256" , type - "JWT"
-            // Payload - "userinformation
-            // signature - "secret
-            // KWJKJWERKJNR.KJBASDKAKJDA.OJAOISDOAIHDA
-            // AUTH0
-            return null;
+
+            JwtTokenProvider jwtTokenProvider = new JwtTokenProvider(encryptedPassword);
+            UserAuthTokenEntity userAuthToken = new UserAuthTokenEntity();
+            userAuthToken.setUser(userEntity);
+
+            final ZonedDateTime now = ZonedDateTime.now();
+            final ZonedDateTime expiresAt = now.plusHours(8);
+            userAuthToken.setAccessToken(jwtTokenProvider.generateToken(userEntity.getUuid(), now, expiresAt));
+
+            userAuthToken.setLoginAt(now);
+            userAuthToken.setExpiresAt(expiresAt);
+            userAuthToken.setCreatedAt(now);
+            userAuthToken.setCreatedBy("backend-api");
+
+            userDao.createAuthToken(userAuthToken);
+            userDao.updateUser(userEntity);
+            userEntity.setLastLoginAt(now);
+
+            return userAuthToken;
+
         }
         else
         {
